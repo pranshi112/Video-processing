f5caac4e1179c9fe347043b06e07b49d88978ed2
Pranshi Jindal <apranshi11@gmail.com>
2021-11-20 03:35:17 +0530

    Add comments

--- Colour_detect.py
+++ Colour_detect.py
@@ -5,30 +5,55 @@ cap = cv2.VideoCapture(0)
 
 while True: 
 
-    _, frame = cap.read()
+    ret, frame = cap.read()
+    
+    # to reduce noise in images, image smoothing is done using cv2.GaussianBlur
     blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    
+    # converting frame to HSV format
+    # cv2.COLOR_BGR2HSV converts from default BGR format to HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Red color
+    
+    # passing the lowest HSV values of red
     low_red = np.array([161, 155, 84])
+    
+    # passing the highest HSV values of red
     high_red = np.array([179, 255, 255])
+    
+    # we use this mask on hsv_frame with values in the passed range
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    
+    # Using only red_mask shows red color in white but using this line, we see red in red
     red = cv2.bitwise_and(frame, frame, mask=red_mask)
 
+    # first argument in cv2.findContours() is source image, second is contour retrieval mode, third is contour approximation method
+    # cv2.RETR_TREE retrieves all possible contours from the image
+    # cv2.CHAIN_APPROX_NONE stores all the points on the boundary(i.e. stores edges) and not just vertices
+    # contours here is a list of all the contours in the image
     contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     
+    # finding area for each contour
     for contour in contours:
         area = cv2.contourArea(contour)
 
         if area > 5000:
+            # first argument is source image, second is the contours from the list, third is index of contours(to draw all contours, pass -1)
+            # remaining arguments are color, thickness
             cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
 
+            # Image moments help to calculate the centroid of the image
             M = cv2.moments(contour)
 
+            # Calculating centroids
             cx = int(M["m10"]/M["m00"])
             cy = int(M["m01"]/M["m00"])
 
+            # finding center by drawing a circle at the center
             cv2.circle(frame,(cx,cy),7,(255,255,255),-1)
+            
+            # putting text of the detected color
             cv2.putText(frame, "Red", (cx-20, cy-20), cv2.FONT_HERSHEY_SIMPLEX,2.5, (255,255,255), 3)
    
 
@@ -46,7 +71,7 @@ while True:
     for contour in contours:
         area = cv2.contourArea(contour)
 
-        if area > 5000:
+        if area > 5000:            
             cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
 
             M = cv2.moments(contour)
@@ -113,6 +138,7 @@ while True:
     cv2.imshow("Frame", frame)
     cv2.imshow("Yellow", yellow)
 
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(1)    
+    # 27 is ASCII code for Esc key
     if key == 27:
         break
